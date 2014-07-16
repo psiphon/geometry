81f4ded1bbd933ebac58342425ba2b5a77fb7bfb
wwhitaker <wwhitaker@yammer-inc.com>
2014-07-16 14:29:18 -0700

    Removed unused import

--- src/main/java/com/sportdataconnect/geometry/Triangle2D.java
+++ src/main/java/com/sportdataconnect/geometry/Triangle2D.java
@@ -1,7 +1,6 @@
 package com.sportdataconnect.geometry;
 
 import com.google.common.collect.ImmutableList;
-import java.util.ArrayList;
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
