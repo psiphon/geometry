737f990bee69e7f03d0accedbd6daa9670b49ab3
wwhitaker <wwhitaker@yammer-inc.com>
2014-08-01 12:27:09 +0100

    Added javadoc to Extrusion3D

--- src/main/java/com/sportdataconnect/geometry/Extrusion3D.java
+++ src/main/java/com/sportdataconnect/geometry/Extrusion3D.java
@@ -5,6 +5,10 @@ import java.util.Collection;
 import java.util.List;
 
 /**
+ * Representation of a 2D shape extruded into 3D space along a central edge. The extrusion consists of a 2D polygon
+ * describing the cross section and 3D start and end points. From this description the start and end face can be described
+ * as points, edges or triangles. The supplied cross section polygon need not be projected in a plane perpendicular to the
+ * central edge - the face dir parameters allows the cross section to be projected onto an arbitrary plane.
  *
  * @author sportdataconnect
  */
@@ -41,6 +45,61 @@ public final class Extrusion3D {
     public Point3D getStartPoint() {
         return startPoint;
     }
+
+    /**
+     * Returns the all edges along the start face of the extrusion.
+     * @return Returns the all edges along the start face of the extrusion
+     */
+    public List<Edge3D> getStartFaceEdges() {
+        List<Edge3D> result = new ArrayList<Edge3D>(facePoints.numPoints());
+        for (int i = 0; i < facePoints.numPoints() - 1; i++) {
+            result.add(new Edge3D(calculateStartFacePoint(i), calculateStartFacePoint(i + 1)));
+        }
+        result.add(new Edge3D(calculateStartFacePoint(facePoints.numPoints() - 1), calculateStartFacePoint(0)));
+        return result;
+    }
+
+    /**
+     * Returns the all edges along the end face of the extrusion with the direction of each edge reversed (the
+     * order of the edge set is preserved from the order of points in the original polygon).
+     * @return Returns the all edges along the start face of the extrusion
+     */
+    public List<Edge3D> getReversedEndFaceEdges() {
+        List<Edge3D> result = new ArrayList<Edge3D>(facePoints.numPoints());
+        for (int i = 0; i < facePoints.numPoints() - 1; i++) {
+            result.add(new Edge3D(calculateEndFacePoint(i + 1), calculateEndFacePoint(i)));
+        }
+        result.add(new Edge3D(calculateEndFacePoint(0), calculateEndFacePoint(facePoints.numPoints() - 1)));
+        return result;
+    }
+
+    /**
+     * Returns the start face broken down into triangles. The tesselation of the 3D triangles is derived from the
+     * tesselation of the originally supplied 2D polygon.
+     * @return Returns the start face broken down into triangles
+     */
+    public Collection<Triangle3D> getStartFaceTriangles() {
+        Collection<Triangle2D> sfc = facePoints.toTriangles();
+        Collection<Triangle3D> result = new ArrayList<Triangle3D>(sfc.size());
+        for (Triangle2D sfcTriangle : sfc) {
+            result.add(startFaceUnprojector.unproject(sfcTriangle));
+        }
+        return result;
+    }
+
+    /**
+     * Returns the start face broken down into triangles. The tesselation of the 3D triangles is derived from the
+     * tesselation of the originally supplied 2D polygon.
+     * @return Returns the start face broken down into triangles
+     */
+    public Collection<Triangle3D> getEndFaceTriangles() {
+        Collection<Triangle2D> efc = facePoints.toTriangles();
+        Collection<Triangle3D> result = new ArrayList<Triangle3D>(efc.size());
+        for (Triangle2D efcTriangle : efc) {
+            result.add(endFaceUnprojector.unproject(efcTriangle));
+        }
+        return result;
+    }
     
     private Unprojector startFaceUnprojector = new Unprojector() {
 
@@ -66,42 +125,6 @@ public final class Extrusion3D {
     
     private Point3D calculateEndFacePoint(final int ixEndFace) {
         return endFaceUnprojector.unproject(facePoints.get(ixEndFace));
-    }
-    
-    public List<Edge3D> getStartFaceEdges() {
-        List<Edge3D> result = new ArrayList<Edge3D>(facePoints.numPoints());
-        for (int i = 0; i < facePoints.numPoints() - 1; i++) {
-            result.add(new Edge3D(calculateStartFacePoint(i), calculateStartFacePoint(i + 1)));
-        }
-        result.add(new Edge3D(calculateStartFacePoint(facePoints.numPoints() - 1), calculateStartFacePoint(0)));
-        return result;
-    }
-    
-    public List<Edge3D> getReversedEndFaceEdges() {
-        List<Edge3D> result = new ArrayList<Edge3D>(facePoints.numPoints());
-        for (int i = 0; i < facePoints.numPoints() - 1; i++) {
-            result.add(new Edge3D(calculateEndFacePoint(i + 1), calculateEndFacePoint(i)));
-        }
-        result.add(new Edge3D(calculateEndFacePoint(0), calculateEndFacePoint(facePoints.numPoints() - 1)));
-        return result;
-    }
-
-    public Collection<Triangle3D> getStartFaceTriangles() {
-        Collection<Triangle2D> sfc = facePoints.toTriangles();
-        Collection<Triangle3D> result = new ArrayList<Triangle3D>(sfc.size());
-        for (Triangle2D sfcTriangle : sfc) {
-            result.add(startFaceUnprojector.unproject(sfcTriangle));
-        }
-        return result;
-    }
-
-    public Collection<Triangle3D> getEndFaceTriangles() {
-        Collection<Triangle2D> efc = facePoints.toTriangles();
-        Collection<Triangle3D> result = new ArrayList<Triangle3D>(efc.size());
-        for (Triangle2D efcTriangle : efc) {
-            result.add(endFaceUnprojector.unproject(efcTriangle));
-        }
-        return result;
     }
     
 }
